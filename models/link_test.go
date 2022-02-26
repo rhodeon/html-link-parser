0096bc5cf4d47d3f78dc172d7ac7192384efde32
rhodeon <ruonabraks@gmail.com>
2022-02-26 02:48:24 +0100

    Added tests for BuildLinks, Links.GetTexts and Links.GetUrls.

---
+++ models/link_test.go
@@ -0,0 +1,95 @@
+package models
+
+import (
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestBuildLinks(t *testing.T) {
+	tests := []struct {
+		name string
+		in   io.Reader
+		want Links
+	}{
+		{
+			name: "single link",
+			in:   strings.NewReader(testHtml["single link"]),
+			want: Links{
+				Link{
+					Url:  "/other-page",
+					Text: "A link to another page",
+				},
+			},
+		},
+		{
+			name: "multiple links",
+			in:   strings.NewReader(testHtml["multiple links"]),
+			want: Links{
+				Link{
+					Url:  "/other-page",
+					Text: "A link to another page",
+				},
+				Link{
+					Url:  "/another-page",
+					Text: "A link to yet another page",
+				},
+			},
+		},
+		{
+			name: "links with nested elements",
+			in:   strings.NewReader(testHtml["links with nested elements"]),
+			want: Links{
+				Link{
+					Url:  "https://www.github.com/rhodeon",
+					Text: "Check me out on GitHub!",
+				},
+				Link{
+					Url:  "https://github.com/rhodeon/html-link-parser",
+					Text: "Self-referencing link!",
+				},
+			},
+		},
+		{
+			name: "link with comment",
+			in:   strings.NewReader(testHtml["link with comment"]),
+			want: Links{
+				Link{
+					Url:  "/cipher",
+					Text: "zero cipher",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := BuildLinks(tt.in)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Got: %#v;\nWant: %#v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestLinks_GetTexts(t *testing.T) {
+	links := testLinks
+	want := []string{"github.com", "reddit.com", ""}
+	got := links.GetUrls()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: %#v;\nWant: %#v", got, want)
+	}
+}
+
+func TestLinks_GetUrls(t *testing.T) {
+	links := testLinks
+	want := []string{"share repository", "", "view profile"}
+	got := links.GetTexts()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got: %#v;\nWant: %#v", got, want)
+	}
+}
---
+++ models/link_test_samples.go
@@ -0,0 +1,60 @@
+package models
+
+var testHtml = map[string]string{
+	"single link": `<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`,
+	"multiple links": `<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+  <a href="/another-page">A link to yet another page</a>
+</body>
+</html>
+`,
+	"links with nested elements": `<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <h1>GitHub</h1>
+    <div>
+        <a href="https://www.github.com/rhodeon">
+            Check me out on <em>GitHub</em>! 
+            <i aria-hidden="true"></i>
+        </a>
+        <a href="https://github.com/rhodeon/html-link-parser">
+            Self-<em>referencing</em> <strong>link</strong>!
+        </a>
+    </div>
+</body>
+</html>
+`,
+	"link with comment": `<html>
+
+<body>
+    <a href="/cipher">zero cipher
+        <!-- commented text SHOULD NOT be included! -->
+    </a>
+</body>
+
+</html>
+`,
+}
+
+var testLinks = Links{
+	Link{
+		Url:  "github.com",
+		Text: "share repository",
+	},
+	Link{
+		Url: "reddit.com",
+	},
+	Link{
+		Text: "view profile",
+	},
+}
